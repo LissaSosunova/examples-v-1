da3229082b2cb065f6fe6c2063c893a8244c64cb
Alisa Sosunova <lissasonova@gmail.com>
2018-11-06 16:47:39 +0200

    public1

--- app/src/page-blocks/layouts/l-about/l-about.controller.js
+++ app/src/page-blocks/layouts/l-about/l-about.controller.js
@@ -2,14 +2,51 @@ app.controller("l-flow.about", function ($scope, $state, $flowDataAbout) {
     $scope.about = {};
 
     function init() {
-        $scope.getInfoAbout = function () {
-            $flowDataAbout.getData()
-                .then(response => {
-                        $scope.about = response.paragraph;
+        // $scope.getInfoAbout = function () {
+        //     $flowDataAbout.getData()
+        //         .then(response => {
+        //                 $scope.about = response.paragraph;
+        //             },
+        //             error => $scope.errorMessage = error);
+        // } ;
+
+        var a = {
+            title: "Идея проекта.",
+            description: "Этот ресурс предназначен для людей, которые не любят скучать. Если вы едете в транспорте, сидите в очереди или просто ненавидите скуку, то вам достаточно иметь устройство с доступом в Интернет и наушники. Наушники скорее для того, что бы не смущать людей, которые вместе с вами коротают это время. Откройте нужный раздел, выберите интересующую вас аудио-книгу или веселое видео и просто нажмите \"play\".",
+            platform: "Angular JS",
+            technologies:
+                [
+                    {
+                        title: "AngularJS",
+                        descr: "ui-router, resource"
                     },
-                    error => $scope.errorMessage = error);
-        } ;
-        $scope.getInfoAbout();
+                    {
+                        title: "JavaScript",
+                        descr: "ecmascript 6"
+                    },
+                    {
+                        title: "MOCK",
+                        descr: "Server"
+                    },
+                    {
+                        title: "npm",
+                        descr: "node-static, express, fs, body-parser"
+                    },
+                    {
+                        title: "Gulp",
+                        descr: "Development"
+                    },
+                    {
+                        title: "CSS",
+                        descr: "Grids, Font Awesome"
+                    },
+                    {
+                        title: "SASS",
+                        descr: "Variables, mixins, functions, type-default, styles"
+                    }
+                ]
+        }
+        $scope.about = a;
     }
     init();
 
--- app/src/page-blocks/layouts/l-header/l-header.controller.js
+++ app/src/page-blocks/layouts/l-header/l-header.controller.js
@@ -1,9 +1,9 @@
 app.controller('l-header', function($scope, $state) {
     $scope.model = {
         images: {
-            pict: "../app/images/top_pict.png",
+            pict: "app/images/top_pict.png",
             logo: {
-                src: "app/images/logo_paragraph_sm.png",
+                src: "app/images/logo.png",
                 alt: "Paragraph",
                 href: "flow"
             }
